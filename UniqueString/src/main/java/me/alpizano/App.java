aea4b7143d2b53a437e40aea2914ad17df85a8da
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-12-14 16:18:36 -0600

    implementing hash table for unique string algorithm

--- UniqueString/src/main/java/me/alpizano/App.java
+++ UniqueString/src/main/java/me/alpizano/App.java
@@ -1,14 +1,26 @@
 package me.alpizano;
 
+import java.util.Hashtable;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public boolean uniqueString() {
+        String base = "I go to eat fudge";
+        Hashtable<String, String> table = new Hashtable<>();
+
+        return true;
+    }
+
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
+
+
+
+
     }
 }
