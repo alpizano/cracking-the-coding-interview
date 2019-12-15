1ab2c36f83d342eb7b90d3d8030146791040c450
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-12-15 02:09:44 -0600

    adding Boolean Array algorithm implementation to determine if String is Unique

---
+++ UniqueString/src/main/java/me/alpizano/BooleanArrayImplementation.java
@@ -0,0 +1,10 @@
+package me.alpizano;
+
+public class BooleanArrayImplementation {
+    public static boolean isUnique() {
+        return true
+    }
+    public static void main(String[] args) {
+        
+    }
+}
