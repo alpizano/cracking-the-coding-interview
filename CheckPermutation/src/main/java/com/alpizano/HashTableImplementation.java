cf8e1daa51830e3d5b444e912b6b6917b2104bb4
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-12-17 06:27:57 -0600

    implemented hashtable algorithm for checking if two strings are permutations of each other

--- CheckPermutation/src/main/java/com/alpizano/HashTableImplementation.java
+++ CheckPermutation/src/main/java/com/alpizano/HashTableImplementation.java
@@ -1,7 +1,29 @@
 package com.alpizano;
 
+
+import java.util.Hashtable;
+
+
 public class HashTableImplementation {
     public static boolean checkPermutation(String str1, String str2) {
+        Hashtable<Character,Integer> table = new Hashtable<>();
+
+        // if not same length, can't be permutation
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        for(int i=0; i < str1.length(); i++) {
+            table.put(str1.charAt(i),i);
+        }
+
+        for(int j=0; j < str2.length(); j++) {
+            if(!table.containsKey(str2.charAt(j))) {
+                return false;
+            }
+        }
+
+        return true;
 
     }
     public static void main(String[] args) {
