f5bbb6a00f3e025f8306784a4cb0e4c826bf75e1
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-12-25 18:10:44 -0600

    successfully implemented URLify algorithm for String

--- URLify/src/main/java/URLify.java
+++ URLify/src/main/java/URLify.java
@@ -2,27 +2,45 @@ import java.util.regex.*;
 
 public class URLify {
 
-    public static void URLify(String str, int length) {
+    public static String URLify(String str, int length) {
         Pattern p = Pattern.compile("[a-zA-Z_0-9]");
 
         char[] charArray = str.toCharArray();
 
-        int arr_placeholder = length-1;
-        int char_placeholder = 0;
+        int first_char = length-1;
+        int arr_pointer = charArray.length-1;
 
-        for(int i=charArray.length-1; i>=0; i--) {
-            if(Pattern.matches(Character.toString(charArray[i]),"[a-zA-Z_0-9]")) {
-                //char_placeholder = i;
-                System.out.println("Hello");
+        for(int i=first_char; i>=0; i--) {
+            // use true length-1 to find first non-space (char) going reverse
+
+            // move first char to end of char array
+            if(Character.toString(charArray[i]).matches("[a-zA-Z_0-9]+")) {
+
+                // Copy value to end of array
+                charArray[arr_pointer] = charArray[i];
+
+                // De-increment array pointer
+                arr_pointer--;
+            }
+            else {
+                charArray[arr_pointer] = '0';
+                arr_pointer--;
+                charArray[arr_pointer] = '2';
+                arr_pointer--;
+                charArray[arr_pointer] = '%';
+                arr_pointer--;
             }
         }
 
-
+        return String.valueOf(charArray);
     }
     public static void main(String[] args) {
-        String str1 = "Mr John Smith      ";
+        String str1 = "Mr John Smith    ";
+        //String str2 = "  Mr%20John%20Smith";
+
         URLify test = new URLify();
-        test.URLify(str1,13);
+       String ret_str =  test.URLify(str1,13);
+       System.out.println(ret_str);
 
     }
 }
