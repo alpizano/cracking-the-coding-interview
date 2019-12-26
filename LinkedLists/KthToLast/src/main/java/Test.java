d5f2ad57bee07721af48624b604118c794ddcd78
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-12-26 15:11:22 -0600

    writing node class and algorithm to iterate thru list and counter to count distance from end/tail

--- LinkedLists/KthToLast/src/main/java/Test.java
+++ LinkedLists/KthToLast/src/main/java/Test.java
@@ -1,2 +1,23 @@
 public class Test {
+    public void kthToLast(Node list, int k) {
+        int counter = 0;
+
+        for(Node cursor = list; cursor != null; cursor = cursor.next) {
+
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello wrold");
+
+        Node head = new Node(69);
+        head.appendToTail(55);
+        head.appendToTail(13);
+        head.appendToTail(42);
+        head.appendToTail(11);
+
+        Test test = new Test();
+        test.kthToLast(head, 3);
+
+    }
 }
