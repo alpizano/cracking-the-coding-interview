d3fd6a44afb582814839061938a5cf7dcfd364d5
ennth <39110859+aemaeth0@users.noreply.github.com>
2020-02-26 00:07:07 -0600

    adding graph base class

--- TreesAndGraphs/RouteBetweenNodes/src/main/java/Graph.java
+++ TreesAndGraphs/RouteBetweenNodes/src/main/java/Graph.java
@@ -1,2 +1,74 @@
-public class Graph {
+public class Graph<E> implements Cloneable {
+    // n x n 2-dimensional array
+    private boolean edges[][];
+    private Object[] labels;
+
+    public Graph(int n) {
+        edges = new boolean[n][n];
+        labels = new Object[n];
+    }
+
+    public Graph<E> clone() {
+        Graph<E> answer;
+        try {
+            answer = (Graph<E>) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("This class does not implement Cloneable");
+        }
+        answer.edges = edges.clone();
+        answer.labels = labels.clone();
+
+        return answer;
+    }
+
+    public void addEdge(int source, int target) {
+        edges[source][target] = true;
+    }
+
+    public boolean isEdge(int source, int target) {
+        return edges[source][target];
+    }
+
+    public void removeEdge(int source, int target) {
+        edges[source][target] = false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E getLabel(int vertex) {
+        return (E) labels[vertex];
+    }
+
+
+    public void setLabel(int vertex, E newLabel) {
+        labels[vertex] = newLabel;
+    }
+
+    public int size() {
+        return labels.length;
+    }
+
+    public int[] neighbors(int vertex) {
+        int i;
+        int count;
+        int[] answer;
+
+        // First count how many edges have the vertex as their source
+        count = 0;
+        for(i=0; i < labels.length; i++) {
+            if (edges[vertex][i])
+                count++;
+        }
+
+        // Allocate the array for the answer
+        answer = new int[count];
+
+        // Fill the array for the answer
+        count = 0;
+        for (i = 0; i < labels.length; i++) {
+            if(edges[vertex][i])
+                answer[count++] = i;
+        }
+
+        return answer;
+    }
 }
