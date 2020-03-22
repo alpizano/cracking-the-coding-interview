f40d5888bab731610e7a3620fa339279b7f03feb
Alberto Pizano <KBRV37@motorolasolutions.com>
2020-03-21 23:44:27 -0500

    adding Arrays and Strings solutions from Cracking the Coding Interview

--- ArraysAndStrings/IsUnique/src/main/java/me/alpizano/HashTableImplementation.java
+++ ArraysAndStrings/IsUnique/src/main/java/me/alpizano/HashTableImplementation.java
@@ -7,20 +7,19 @@ public class HashTableImplementation {
         Hashtable<String, Integer> table = new Hashtable<>();
 
         for(int i=0; i< s.length() ; i++) {
-            if(table.containsKey(Character.toString(s.charAt(i)))) {
-                return false;
-            }
-            else {
-                table.put(Character.toString(s.charAt(i)), i);
+            if(i != 0 ) {
+                if (table.containsKey(Character.toString(s.charAt(i)))) {
+                    return false;
+                } else {
+                    table.put(Character.toString(s.charAt(i)), i);
+                }
             }
         }
-
-//        for (String key : table.keySet()) {
-//
-//        }
         return true;
     }
+
     public static void main(String[] args) {
+        // testing two string; positive case and negative case
         String notUnique = "I go to eat fudge";
         String Unique = "abcdefghijklmnopqrstuvwxyz";
 
